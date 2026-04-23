e78364e7d22e15f7f59d53f544c11594ebbf6085
i.a.grekhov <ivan.grekhov@moneyherogroup.com>
2026-04-23 23:50:41 +0500

    Create PreviewPage

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,26 +5,4 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  pdfSource: string | Uint8Array | null = null;
-  urlInput = "";
-
-  onFileSelected(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    const file = input.files && input.files[0];
-    if (!file) {
-      return;
-    }
-    const reader = new FileReader();
-    reader.onload = () => {
-      const buffer = reader.result as ArrayBuffer;
-      this.pdfSource = new Uint8Array(buffer);
-    };
-    reader.readAsArrayBuffer(file);
-  }
-
-  onUrlChanged(value: string): void {
-    this.urlInput = value;
-    this.pdfSource = value ? value : null;
-  }
-}
+export class AppComponent {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,16 @@ import { CommonModule } from "@angular/common";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PdfPreviewComponent } from "./components/pdf-preview/pdf-preview.component";
+import { PdfPreviewPage } from "./pages/pdf-preview/pdf-preview.page";
+import { ImportFileComponent } from "./components/import-file/import-file.component";
 
 @NgModule({
-  declarations: [AppComponent, PdfPreviewComponent],
+  declarations: [
+    AppComponent,
+    PdfPreviewComponent,
+    PdfPreviewPage,
+    ImportFileComponent,
+  ],
   imports: [BrowserModule, CommonModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
